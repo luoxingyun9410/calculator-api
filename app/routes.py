249116d34a9301b8c35e63f98cd90963b54bfc5e
luoxingyun9410 <stevenluo0514@gmail.com>
2025-12-02 09:44:30 +1100

    home / route updated /postman tested

--- app/routes.py
+++ app/routes.py
@@ -28,19 +28,56 @@ def add_route(a, b):
         return jsonify({"result": result})
     except ValueError:
         return jsonify({"error": "Please provide either a float or integer"}), 400
+    
+@bp.route("/subtract")
+def subtract_home():
+    return jsonify({"Addition": "Please provide two numbers like /add/1/2"})
 
-@bp.route("/subtract/<int:a>/<int:b>")
+@bp.route("/subtract/<a>/<b>")
 def subtract_route(a, b):
-    return jsonify({"result": subtract(a,b)})
+    try:
+        aValue = float(a)
+        bValue = float(b)
 
-@bp.route("/multiply/<int:a>/<int:b>")
+        result = subtract(aValue,bValue)
+        if result.is_integer():
+            result = int(result)
+        return jsonify({"result": result})
+    except ValueError:
+        return jsonify({"error": "Please provide either a float or integer"}), 400
+
+@bp.route("/multiply")
+def multiply_home():
+    return jsonify({"Multiplication": "Please provide two numbers like /multiply/1/2"})
+
+@bp.route("/multiply/<a>/<b>")
 def multiply_route(a, b):
-    return jsonify({"result": multiply(a,b)})
+    try:
+        aValue = float(a)
+        bValue = float(b)
 
-@bp.route("/divide/<int:a>/<int:b>")
+        result = multiply(aValue,bValue)
+        if result.is_integer():
+            result = int(result)
+        return jsonify({"result": result})
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 400
+
+@bp.route("/divide")
+def divide_home():
+    return jsonify({"Division": "Please provide two numbers like /divide/1/2"})
+
+@bp.route("/divide/<a>/<b>")
 def divide_route(a, b):
     try:
-        result = divide(a,b)
+        aValue = float(a)
+        bValue = float(b)
+
+        result = divide(aValue,bValue)
+        if result.is_integer():
+            result = int(result)
         return jsonify({"result": result})
+    except ZeroDivisionError:
+        return jsonify({"error": "Division by zero is not allowed"}), 400
     except ValueError as e:
         return jsonify({"error": str(e)}), 400
